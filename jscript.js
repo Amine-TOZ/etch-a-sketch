f605a78ff4a47445014643ed255e4c00d2f08832
Amine <183746156+Amine-TOZ@users.noreply.github.com>
2024-12-10 17:55:14 +0100

    Add button that change the grid size.
    added a button and an event listener, when clicked
    it prompts the user to enter a size,the old grid
    gets deleted and the a new one with the typed size
    gets created.

--- jscript.js
+++ jscript.js
@@ -6,13 +6,27 @@ let container = document.getElementById("container");
 }
 
 container.addEventListener("mouseover",(event)=>{
-   event.target.style.backgroundColor="green";
+   event.target.style.backgroundColor="black";
 
 })
 
 let button = document.getElementsByTagName("button")
 
 button[0].addEventListener("click",()=>{
-   let prompt = prompt("🤔");
-   
+    let promptInput = prompt("🤔");
+    const promptInputNum = +promptInput
+    if(promptInputNum <= 100 && promptInputNum !== NaN && promptInputNum > 0){
+      let childDiv = container.querySelectorAll(".childDiv");
+          childDiv.forEach(element => {
+              element.remove();
+          });
+    for(let i = 0; i < (promptInputNum*promptInputNum); i++){
+          div = document.createElement("div");
+          container = document.getElementById("container");
+          div.setAttribute("class","childDiv");
+          container.appendChild(div);
+       }
+       container.style.width = `${promptInputNum*8}px`
+       container.style.height = `${promptInputNum*8}px`
+    }else {alert("You need to enter a number < 100")}   
 })
